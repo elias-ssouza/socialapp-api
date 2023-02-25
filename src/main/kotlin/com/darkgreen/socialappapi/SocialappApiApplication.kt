3193fac75d8196ffcc364812d11fbff38e261119
elias-ssouza <elias.ssouza@hotmail.com>
2023-02-24 23:41:18 -0300

    Initial commit

---
+++ src/main/kotlin/com/darkgreen/socialappapi/SocialappApiApplication.kt
@@ -0,0 +1,11 @@
+package com.darkgreen.socialappapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialappApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialappApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/darkgreen/socialappapi/SocialappApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.darkgreen.socialappapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialappApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
