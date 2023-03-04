9ac6058fb6425dc51cab88ef7671699e605baf11
elias-ssouza <elias.ssouza@hotmail.com>
2023-03-03 23:59:13 -0300

    iniciando spring security

---
+++ src/main/kotlin/com/darkgreen/socialappapi/configuration/WebSecurityConfiguration.kt
@@ -0,0 +1,8 @@
+package com.darkgreen.socialappapi.configuration
+
+import org.springframework.context.annotation.Configuration
+
+@EnableSecurity
+@Configuration
+class WebSecurityConfiguration {
+}
